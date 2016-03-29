23f874726172731a2c5ea5e1b6a62062dab2dde4
Aman Mittal <amandeepmittal@live.com>
2016-03-29 13:03:10 +0530

    home.html +

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -4,6 +4,9 @@ var app = angular.module('myblogapp', ['ngRoute'])
 // Configuration-Routing
 app.config(function($routeProvider){
   $routeProvider
+    .when("/",{
+      templateUrl:'home.html'
+    })
     .when("/about",{
       templateUrl: 'about.html'
     })
