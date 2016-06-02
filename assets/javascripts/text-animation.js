6c4c0858b3fd1a3000a18d5b3ace7bc13dd8e042
Aman Mittal <amandeepmittal@live.com>
2016-06-02 11:53:11 +0530

    Create text-animation.js

---
+++ assets/javascripts/text-animation.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $(".typing").typed({
+    strings: ["Javascript.",
+    "Node.",
+    "Angular.",
+    "Mongo.",
+    "Express.",
+    "MEAN Stack.",
+    "Heroku.",
+    "Mongoose.",
+    "Tomcat.",],
+    typeSpeed: 100,
+    backSpeed: 155,
+    startDelay: 850,
+    loop: true
+  });
+});
