d527a57108adb4972a3b838ffd656befd764cbd8
amandeepmittal <amandeepmittal@live.com>
2018-01-31 12:35:18 +0530

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-1bd2912de9ffc9def557.js","1872789963a54e2d7a1e0cf37344d358"],["/commons-c1884cdbe5e450b37645.js","13775e130cc35a3fefba7d73ce28622c"],["/index.html","a3d23cba01a118a7fdbd7c4e26992820"],["/manifest.json","984061bf3acb8c2b26ce58cc4dc911ef"],["/offline-plugin-app-shell-fallback/index.html","30f39662ca10644f614e5afd6a939e89"]];
+var precacheConfig = [["/app-1bd2912de9ffc9def557.js","1872789963a54e2d7a1e0cf37344d358"],["/commons-c1884cdbe5e450b37645.js","13775e130cc35a3fefba7d73ce28622c"],["/index.html","4da35768db07301b368948b3c68e98e1"],["/manifest.json","984061bf3acb8c2b26ce58cc4dc911ef"],["/offline-plugin-app-shell-fallback/index.html","20de795a9be961828b21b94b1d55a05b"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
