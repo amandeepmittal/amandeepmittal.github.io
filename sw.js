946df9efd23c369a4b58fc392484795da4c600b3
Aman Mittal <amandeepmittal@live.com>
2018-06-27 16:39:23 +0530

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-1bd2912de9ffc9def557.js","1872789963a54e2d7a1e0cf37344d358"],["/app-76077d2a647ffefb5bd1.js","3c84f281c2720be2af2a743749830c7f"],["/commons-c1884cdbe5e450b37645.js","13775e130cc35a3fefba7d73ce28622c"],["/index.html","4729f6d51082961e97d542c455b8220f"],["/manifest.json","95e40ad1198fc65aa3e74de367fac26b"],["/offline-plugin-app-shell-fallback/index.html","f485198de6445a9d74156b35c49ec08c"]];
+var precacheConfig = [["/app-1bd2912de9ffc9def557.js","1872789963a54e2d7a1e0cf37344d358"],["/app-76077d2a647ffefb5bd1.js","3c84f281c2720be2af2a743749830c7f"],["/commons-c1884cdbe5e450b37645.js","13775e130cc35a3fefba7d73ce28622c"],["/index.html","dd01a92a750da432011bad399ce3a598"],["/manifest.json","95e40ad1198fc65aa3e74de367fac26b"],["/offline-plugin-app-shell-fallback/index.html","a1267c72f3bb4c8ddcaecb94bf374e74"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
